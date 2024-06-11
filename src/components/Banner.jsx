f3372c8a073b9b7c025c0ee043247f6e2011d61c
Surya Narayana Gaddam <144539989+Suryanarayana-gaddam@users.noreply.github.com>
2024-06-11 22:41:10 +0530

    changed style of bannercard

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -10,9 +10,9 @@ const Banner = () => {
 
   return (
     <div className='px-4 lg:px-24 bg-teal-100 flex items-center'>
-        <div className='flex w-full flex-col md:flex-row justify-between items-center gap-12 pt-40 pb-20'>
+        <div className='flex w-full flex-col md:flex-row justify-around items-center gap-12 pt-40 pb-20'>
             {/* left side */}
-            <div className='md:w-1/2 space-y-8 h-f'>
+            <div className='md:w-3/5 space-y-8 h-f'>
               <h2 className='text-5xl font-bold leading-snug text-black'>Buy And Sell Your Books <span className='text-blue-700 '>for the best prices</span></h2>
               <p className='md:w-4/5'>
                 Welcome to Book Store, your haven for literary treasures! Dive into our curated selection of books, where each title promises an adventure, a lesson, or a moment of pure delight. From bestsellers to hidden gems, we&apos;ve got your next favorite read waiting. Explore our virtual shelves and let the stories unfold. Your next literary escape awaits at Book Store.
