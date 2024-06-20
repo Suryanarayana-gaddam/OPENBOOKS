0f3ba7f79a90bbcecbea0127f59b5cce8035fa12
Surya Narayana Gaddam <144539989+Suryanarayana-gaddam@users.noreply.github.com>
2024-06-20 11:48:07 +0530

    updated some code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -115,7 +115,7 @@ const Navbar = () => {
 return (
     <header className='w-full bg-transparent fixed top-0 left-0 right-0 transition-all ease-in duration-300'>
         <nav className={`py-4 lg:px-14 px-4 ${isSticky ? "sticky top-0 left-0 right-0 bg-blue-300" : ""}`}>
-            <div className='flex justify-between items-center text-base lg:gap-8 sm:gap-4'>
+            <div className='flex justify-between items-center text-base gap-8'>
                 {/* logo */} 
                 <Link to="/" className='text-2x1 font-bold text-blue-700 flex items-center gap-2'><FaBookAtlas className='inline-block text-3xl ml-2'/><span className='text-3xl font-serif'>ReadIt</span></Link>
                 {/*nav items for large device */}
@@ -175,6 +175,7 @@ return (
                     </button>
                 </div>
                 <div className='md:hidden sm:hidden flex ' >
+                <div>
                 <Link to="/userProfile">
                 {user? (user.photoURL ? (<img src={user?.photoURL} alt="" className='p-0 h-8 w-8 border-none rounded-full' />) : <img src={profilePic} alt="Profile" className='p-0 h-8 w-8 border-none rounded-full' />
 ) : <FaUser className='p-0 mt-1 mr-1 h-4 w-4 border-none rounded-full' />}
@@ -182,6 +183,7 @@ return (
                     <button onClick={toggleUserMenu} className="text-black focus:outline-none">
                     <FaBarsStaggered className="h-5 w-5 text-black" />
                     </button>
+                </div>
                     <div className={`absolute top-10 right-0 z-10 mt-2 py-2 bg-white rounded-lg shadow-md text-left ${isUserMenuOpen ? "block" : "hidden"}`}>
                             {userMenuItems.map(({ link, path }) => (
                                 <Link key={path} to={path} onClick={CloseUserMenu} className=" block p-0 px-4 py-2 text-base text-black  bg-white hover:bg-gray-200 ">
