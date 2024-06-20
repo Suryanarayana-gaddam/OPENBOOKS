d3698d9a3a1a89fbade8780093342f9605d4290c
Surya Narayana Gaddam <144539989+Suryanarayana-gaddam@users.noreply.github.com>
2024-06-20 12:50:49 +0530

    changed the logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -115,10 +115,10 @@ const Navbar = () => {
 
 return (
     <header className='w-full bg-transparent fixed top-0 left-0 right-0 transition-all ease-in duration-300'>
-        <nav className={`py-4 lg:px-14 px-4 sm:px-0 ${isSticky ? "sticky top-0 left-0 right-0 bg-blue-300" : ""}`}>
+        <nav className={`py-4 lg:px-14 px-4 ${isSticky ? "sticky top-0 left-0 right-0 bg-blue-300" : ""}`}>
             <div className='flex justify-between items-center text-base gap-8'>
                 {/* logo */} 
-                <Link to="/" className='text-2x1 font-bold text-blue-700 flex items-center gap-2'><FaBookAtlas className='inline-block text-3xl ml-2'/><span className='text-3xl font-serif'>ReadIt</span></Link>
+                <Link to="/" className='text-2x1 font-bold text-blue-700 flex items-center gap-2'><FaBookAtlas className='inline-block text-3xl ml-2'/><div className='imglogo text-transparent w-40 h-14 rounded-full'>Logo</div></Link>
                 {/*nav items for large device */}
                 <ul className='md:flex space-x-12 hidden'>
                     {
