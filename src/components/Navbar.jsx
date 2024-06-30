05491ad711aa30326e5c55f237b1c951fd9186a9
Surya Narayana Gaddam <144539989+Suryanarayana-gaddam@users.noreply.github.com>
2024-06-30 22:26:25 +0530

    changed logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { FaBarsStaggered, FaBookAtlas, FaUser, FaXmark} from "react-icons/fa6"
 import { AuthContext } from '../context/AuthProvider';
 import Logout from './Logout';
 import useCart from '../../hooks/useCart';
-import "../home/BannerCard.css"
+import "../nav.css"
 const Navbar = () => {
     const [activeItem, setActiveItem] = useState('');
     const [isMenuOpen,setIsMenuOpen] = useState(false);
