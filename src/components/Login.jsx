4556283f0cdaacbd1e7fd72a3f2b07fd2b81b22e
Surya Narayana Gaddam <144539989+Suryanarayana-gaddam@users.noreply.github.com>
2024-07-06 21:36:15 +0530

    auto complete off at login email

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -117,7 +117,7 @@ const handleRegister = () => {
                     <div className="divide-y divide-gray-200">
                         <form onSubmit={handleLogin} className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7 transition-4s">
                             <div className="relative">
-                                <input  id="email" name="email" type="email" className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" required/>
+                                <input  id="email" autoComplete='off' autoFocus name="email" type="email" className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" required/>
                                 
                             </div>
                             <div className="relative">
