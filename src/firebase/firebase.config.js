5f341b3b5febf0c953d84f08e6c92bd3a6756a6f
Surya Narayana Gaddam <144539989+Suryanarayana-gaddam@users.noreply.github.com>
2024-07-10 19:42:42 +0530

    changed the visibility of some details

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "mern-book-inventory-app.firebaseapp.com",
   projectId: "mern-book-inventory-app",
   storageBucket: "mern-book-inventory-app.appspot.com",
