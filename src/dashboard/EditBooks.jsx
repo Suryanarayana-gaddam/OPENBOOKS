6c129fb62ffd1b78e4d00d55a40110587a77aedc
Surya Narayana Gaddam <144539989+Suryanarayana-gaddam@users.noreply.github.com>
2024-07-18 22:30:37 +0530

    styled dashboard users and some others

--- src/dashboard/EditBooks.jsx
+++ src/dashboard/EditBooks.jsx
@@ -60,19 +60,19 @@ const updateBookObj = {
           <span className="ml-2">Back</span>
         </Link>
         <h2 className='mb-8 text-3xl font-bold'>Update the book data</h2>
-        <div className='w-full sm:max-w-[760px] md:max-w-[1014px] lg:max-w-[1270px]'>
-        <form className=" inline-block" onSubmit={handleUpdate}>
+        <div className='w-auto block border sm:max-w-[760px] md:max-w-[1014px] lg:max-w-[1270px]'>
+        <form className="flex max-w-full w-auto flex-col flex-wrap" onSubmit={handleUpdate}>
           {/* First Row */}
           {/* Book Name */}
-          <div className=' gap-8 sm:max-w-[750px] md:max-w-[1004px] lg:max-w-[1260px]'>
-          <div className=''>
+          <div className='flex gap-8 w-auto border sm:max-w-[750px] md:max-w-[1004px] lg:max-w-[1260px]'>
+          <div className='w-1/2 block'>
             <div className="mb-2 block">
               <Label htmlFor="bookTitle" value="Book Title" />
             </div>
             <TextInput id="bookTitle" name="bookTitle" type="text" placeholder="Enter the Book Name" required defaultValue={bookTitle} />
           </div>
           {/* Author Name */}
-          <div className=''>
+          <div className='w-1/2 block'>
             <div className="mb-2 block">
               <Label htmlFor="authorName" value="Author Name" />
             </div>
@@ -81,15 +81,15 @@ const updateBookObj = {
           </div>
           {/* 2 nd Row */}
           {/* Image URL */}
-          <div className=' gap-8 sm:max-w-[750px] md:max-w-[1004px] lg:max-w-[1260px]'>
-            <div className=''>
+          <div className='flex gap-8 w-auto border sm:max-w-[750px] md:max-w-[1004px] lg:max-w-[1260px]'>
+            <div className='w-1/2 block'>
               <div className="mb-2 block"> 
                 <Label htmlFor="imageURL" value=" Book Image URL" /> 
               </div>
               <TextInput id="imageURL" name="imageURL" type="text" placeholder="Enter the Book Image URL" required defaultValue={imageURL} />
             </div>
             {/* Category */}
-            <div className=''>
+            <div className='w-1/2 block'>
               <div className="mb-2 block"> 
                 <Label htmlFor="category" value=" Book Category" /> 
               </div>
@@ -105,21 +105,21 @@ const updateBookObj = {
           </div>
           {/* Thrd Row */}
           {/* Book Description */}
-          <div className=' sm:max-w-[750px] md:max-w-[1004px] lg:max-w-[1260px]'>
+          <div className='w-auto block border sm:max-w-[750px] md:max-w-[1004px] lg:max-w-[1260px]'>
             <div className="mb-2 block">
               <Label htmlFor="bookDescription" value="Book Description" />
             </div>
             <Textarea id="bookDescription" name='bookDescription' placeholder="Write your Book Description..." required className='w-full' rows={6} defaultValue={bookDescription} />
           </div>
           {/* Book Pdf URL */}
-          <div className=' sm:max-w-[750px] md:max-w-[1004px] lg:max-w-[1260px]'>
+          <div className='w-auto block sm:max-w-[750px] md:max-w-[1004px] lg:max-w-[1260px]'>
             <div className="mb-2 block">
               <Label htmlFor="bookPDFURL" value="Book PDF URL" />
             </div>
             <TextInput id="bookPDFURL" name='bookPDFURL' type="text" placeholder="book pdf url" required defaultValue={bookPDFURL} />
           </div>
           {/* Book Price*/}
-          <div className=' sm:max-w-[750px] md:max-w-[1004px] lg:max-w-[1260px]'>
+          <div className='w-auto block sm:max-w-[750px] md:max-w-[1004px] lg:max-w-[1260px]'>
             <div className="mb-2 block">
               <Label htmlFor="bookPrice" value="Book Price" />
             </div>
