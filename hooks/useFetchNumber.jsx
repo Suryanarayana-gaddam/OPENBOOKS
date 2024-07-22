85b07910efa3c60407b372d6f88355aa24226354
Surya Narayana Gaddam <144539989+Suryanarayana-gaddam@users.noreply.github.com>
2024-07-22 11:53:20 +0530

    Clearing errors at About and Added useFetchNumber

--- hooks/useFetchNumber.jsx
+++ hooks/useFetchNumber.jsx
@@ -22,11 +22,8 @@ const useFetchNumber = (url) => {
                 }
 
                 const count = await response.text(); // Assuming the response is a plain number
-                const resparsedCount = JSON.parse(response); // Parse the count to an integer
-                const parsedCount = JSON.parse(count); // Parse the count to an integer
-                console.log("resparsedCount :",resparsedCount.count)
+                const parsedCount = JSON.parse(response); // Parse the count to an integer
                 console.log("parsedCount :",parsedCount.count)
-                console.log("text parsedCount :",count)
                 setData(parsedCount);
             } catch (error) {
                 setError(error.message);
