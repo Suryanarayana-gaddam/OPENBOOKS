9eef7187610a4204339713f75c60c0c5f143b7a0
Surya Narayana Gaddam <144539989+Suryanarayana-gaddam@users.noreply.github.com>
2024-07-28 11:27:02 +0530

    updated the functionality

--- src/main.jsx
+++ src/main.jsx
@@ -12,11 +12,10 @@ import {
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
       <AuthProvider>
         <QueryClientProvider client={queryClient}>
           <RouterProvider router={router} />
       </QueryClientProvider>
       </AuthProvider>
-  </React.StrictMode>,
 )
